c673e7def7d96ba159a5f68c72e3d99876775d4f
kouganepola <koumudiganepola.15@cse.mrt.ac.lk>
2023-04-12 23:30:18 +0200

    [BDM][P1] dolibarr invoker init
    Signed-off-by: kouganepola <koumudiganepola.15@cse.mrt.ac.lk>

---
+++ BDM/collector/configs.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+dolibarr = {
+    "server" : 'localhost',
+    "port": 8000,
+    "api_key" : "9c85e991391b679d0ac67573cb5d6909f169f5dc"
+}
+
+odoo = {
+    "server_url" : "https://demo.odoo.com/start"
+}
--- BDM/collector/invoke.py
+++ BDM/collector/invoke.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python
+
 import xmlrpc.client
 import json
 from abc import abstractmethod
 from dolibarr import Dolibarr
+import configs
 
 class Invoker:
 
@@ -17,6 +20,10 @@ class Invoker:
 class DolibarrInvoker(Invoker):
 
     def __init__(self):
+
+        dolibarr_inst = Dolibarr('http://{server}:{port}/api/index.php/'.format(server=configs.dolibarr["server"], port=configs.dolibarr["port"]), configs.dolibarr["api_key"])
+        product_dict = dolibarr_inst.call_list_api('products')
+        print(product_dict)
         super(Invoker, self).__init__()
 
 
@@ -24,24 +31,24 @@ class OdooInvoker(Invoker):
 
     def __init__(self):
 
-        info = xmlrpc.client.ServerProxy('https://demo.odoo.com/start').start()
+        info = xmlrpc.client.ServerProxy(configs.odoo['server_url']).start()
         url, db, username, password = info['host'], info['database'], info['user'], info['password']
 
-        common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+        common = xmlrpc.client.ServerProxy('{url}/xmlrpc/2/common'.format(url = url))
         common.version()
         uid = common.authenticate(db, username, password, {})
 
         super().__init__(url, uid, password)
         self._db = db
 
-    def query(self):
+    def query(self, model, filter, projection):
 
-        models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(self._uri))
-        result = models.execute_kw(self._db, self._username, self._password, 'res.partner', 'search_read',[[['is_company', '=', True]]], {'fields': ['name', 'country_id', 'comment'], 'limit': 5})
+        models = xmlrpc.client.ServerProxy('{url}/xmlrpc/2/object'.format(url = self._uri))
+        result = models.execute_kw(self._db, self._username, self._password, model, 'search_read',[[filter]], projection )
 
         with open('data.json', 'w') as f:
             json.dump(result, f,indent=4)
 
-
+# mode = DolibarrInvoker()
 mode = OdooInvoker()
-mode.query()
+mode.query('report.stock.quantity', ['state', '!=', 'forecast'],{'fields': ['id', 'company_id', 'date', 'display_name','product_id','product_qty','state'], 'limit': 5})
