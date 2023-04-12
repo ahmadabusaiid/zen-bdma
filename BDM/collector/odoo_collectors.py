283b5a7ec0e359c29164b3ec5a18e9d72bd55bda
Ahmad AS <a.schigdar@gmail.com>
2023-04-13 01:01:34 +0200

    Updated odoo collector

--- BDM/collector/odoo_collectors.py
+++ BDM/collector/odoo_collectors.py
@@ -4,53 +4,51 @@ oi = OdooInvoker()
 
 # inventory stock collector
 
-# model_name = 'stock.report'
-# filters = [
-#     ['state', '=', 'confirmed']
-# ]
-# features = [
-#     'company_id',
-#     'display_name',
-#     'categ_id',  
-#     'creation_date', 
-#     'cycle_time', 
-#     'date_done',
-#     'delay',
-#     'is_backorder',
-#     'is_late',
-#     'partner_id',
-#     'picking_id',
-#     'product_id',
-#     'product_qty',
-#     'scheduled_date',
-#     'stock_value'
-# ]
-# limit = 1000
-
-# oi.query(model = model_name, filter = filters, features = features, limit = limit)
-
-
-
-# POS collector
-
-model_name = 'pos.order.line'
+model_name = 'stock.report'
 filters = [
-    ['price_subtotal', '>', 0]
+    ['state', '=', 'confirmed']
 ]
 features = [
     'company_id',
-    'create_date',
-    'currency_id' 
-    # 'invoiced', 
-    # 'partner_id', 
-    # 'pos_categ_id',
-    # 'total_discount',
-    # 'price_sub_total',
-    # 'price_total',
-    # 'product_categ_id',
-    # 'product_id',
-    # 'product_qty',
-    # 'session_id'
+    'display_name',
+    'categ_id',  
+    'creation_date', 
+    'cycle_time', 
+    'date_done',
+    'delay',
+    'is_backorder',
+    'is_late',
+    'partner_id',
+    'picking_id',
+    'product_id',
+    'product_qty',
+    'scheduled_date',
+    'stock_value'
+]
+limit = 1000
+
+oi.query(model = model_name, filter = filters, features = features, limit = limit)
+
+
+
+# Invoice collector
+
+model_name = 'account.invoice.report'
+filters = [
+    ['state', '=', 'posted']
+]
+features = [
+    'company_id',
+    'country_id',
+    'account_id', 
+    'invoice_date', 
+    'invoice_user_id',
+    'product_id',
+    'product_categ_id',  
+    'quantity',
+    'price_subtotal',
+    'price_total',
+    'partner_id'
 ]
 limit = 1000
 
