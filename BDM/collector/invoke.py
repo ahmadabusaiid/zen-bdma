9b5e30eb15fb71d808d28bd738ec9f26f24f78a2
kouganepola <koumudiganepola.15@cse.mrt.ac.lk>
2023-04-14 16:31:31 +0200

    [BDM][P1] resolve conflicts
    Signed-off-by: kouganepola <koumudiganepola.15@cse.mrt.ac.lk>

--- BDM/collector/invoke.py
+++ BDM/collector/invoke.py
@@ -5,10 +5,6 @@ import json
 from abc import abstractmethod
 from dolibarr import Dolibarr
 import configs
-<<<<<<< Updated upstream
-=======
-#import request
->>>>>>> Stashed changes
 import requests
 
 class Invoker:
@@ -28,23 +24,11 @@ class DolibarrInvoker(Invoker):
         host_path = configs.dolibarr["host_path"]
         self._dolibarr_inst = Dolibarr(f'http://{host_path}/api/index.php/'.format(server=configs.dolibarr["host_path"]), configs.dolibarr["api_key"])
 
-<<<<<<< Updated upstream
-        dolibarr_inst = Dolibarr('http://{server}:{port}/api/index.php/'.format(server=configs.dolibarr["server"], port=configs.dolibarr["port"]), configs.dolibarr["api_key"])
-        super().__init__(dolibarr_inst, None, None)
-    
-    def query(self, model):
-
-        product_dict = self._uri.call_list_api(model) #ex: 'products'
-        with open('data_dolibarr.json'.format(), 'w') as f:
-            json.dump(product_dict, f,indent=4)
-
-=======
         super(Invoker, self).__init__()
     def query(self, model): 
         result = self._dolibarr_inst.call_list_api(model)
         with open(f'{model}.json', 'w') as f:
             json.dump(result, f, indent = 4)
->>>>>>> Stashed changes
 
 class OdooInvoker(Invoker):
 
@@ -90,12 +74,7 @@ class WeatherAPIInvoker(Invoker):
 
         result = requests.get('{host_url}/{path}'.format(host_url = self._uri, path = path), params = params)
 
-<<<<<<< Updated upstream
         file = path.split('.')
         with open(f'{file[0]}.json', 'w') as f:
             json.dump(result.json(), f, indent = 4)
-=======
-        
-#WeatherAPIInvoker().query()
->>>>>>> Stashed changes
 
