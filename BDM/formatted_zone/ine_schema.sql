51bd342bd477d0440fe72343778f7648b2e1da49
Alaa Almutawa <18681041+AlaaAlmutawa@users.noreply.github.com>
2023-06-05 16:30:05 +0200

    Add files via upload

---
+++ BDM/formatted_zone/ine_schema.sql
@@ -0,0 +1,11 @@
+CREATE SCHEMA demographics;
+
+CREATE TABLE population_details (
+    sid string PRIMARY KEY,
+    for_year integer NOT NULL, 
+    date date NOT NULL,
+    cod string NOT NULL,
+    region string NOT NULL, 
+    segment string NOT NULL, 
+    population_count float NOT NULL
+)
