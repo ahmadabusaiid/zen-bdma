aad8f16c2517128c324b0b82e16643bf698139c1
Alaa Almutawa <18681041+AlaaAlmutawa@users.noreply.github.com>
2023-06-05 13:39:57 +0200

    Add files via upload

---
+++ BDM/formatted_zone/weather_schema.sql
@@ -0,0 +1,26 @@
+
+CREATE SCHEMA weather; 
+
+CREATE TABLE weather.forecast (
+    sid string PRIMARY KEY --name+forecast.forecastday.date
+    city_name string,
+    country string,
+    region string, 
+    latitude float,
+    longigtude float, 
+    time_zone string, 
+    date date, --forecast.forecastday.date
+    max_temp float, --forecast.forecastday.day.maxtemp_c
+    min_temp float, --forecast.forecastday.day.mintemp_c 
+    avg_temp float, 
+    max_wind float, 
+    total_precip float, 
+    total_snow float,
+    avg_vis float, 
+    avg_humidity float, 
+    daily_will_it_rain boolean, 
+    daily_will_it_snow boolean, 
+    daily_chance_of_snow boolean, 
+    uv float, 
+    condition string
+); 
